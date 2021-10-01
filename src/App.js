ba287fc5dbbeeb230692ac3916d48486c0b6a99a
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-10-01 07:50:35 +0100

    Class-based Components & Context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import UserFinder from "./components/UserFinder";
 // import Users from "./components/Users";
+import UsersContext from "./store/users-context";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS,
+  };
+
   return (
     <div>
       {/* <Users /> */}
-      <UserFinder />
+      <UsersContext.Provider value={usersContext}>
+        <UserFinder />
+      </UsersContext.Provider>
     </div>
   );
 }
